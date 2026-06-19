fd0bddf2b3f47f61f1811c6f2cf280211d13fd3b
S-entinel <suvobaidya91@gmail.com>
2026-06-19 14:53:16 +0100

    change basket functions to handle duplicate items

--- Level-2/task_two.py
+++ Level-2/task_two.py
@@ -13,14 +13,41 @@ basket = []
 
 
 def add_to_basket(item: dict) -> list:
-    pass
+    """Add an item to the basket and return the updated basket."""
+    for existing_item in basket:
+        if existing_item["name"] == item["name"] and \
+                existing_item["price"] == item["price"]:
+            existing_item["count"] += 1
+            return basket
 
+    basket.append({"name": item["name"], "price": item["price"], "count": 1})
+    return basket
 
 
 def generate_receipt(basket: list) -> str:
-    pass
+    """Generate a formatted receipt string from a list of basket items."""
+    if not basket:
+        print(len(basket))
+        return "Basket is empty"
 
-#####
+    lines = []
+    total_cost = 0
+
+    for item in basket:
+        item_name = item["name"]
+        item_price = item["price"]
+        item_count = item["count"]
+
+        multiple_items_cost = item_price * item_count
+        total_cost += multiple_items_cost
+
+        item_price_str = "Free" if item_price == 0 else f"£{multiple_items_cost:.2f}"
+        lines.append(f"{item_name} x {item_count} - {item_price_str}")
+
+    lines.append(f"Total: £{total_cost:.2f}")
+    return "\n".join(lines)
+
+# cle
 #
 # COPY YOUR CODE FROM LEVEL 1 ABOVE
 #
